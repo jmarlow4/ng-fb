5a8e05665ac88d76e8bfa559382810b5a03eb2d1
Jason Marlow <jasonmarlow@gmail.com>
2022-04-12 18:38:15 -0600

    removed api keys

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,12 +2,12 @@
 export const environment = {
   production: true,
   firebaseConfig: {
-    apiKey: 'AIzaSyCJNfM94G_rR-qt0DYznk0fV6M1UojIwl8',
-    authDomain: 'firebrase.firebaseapp.com',
-    projectId: 'firebrase',
-    storageBucket: 'firebrase.appspot.com',
-    messagingSenderId: '305350835075',
-    appId: '1:305350835075:web:62813dcf8fe37d81a554b6',
-    measurementId: 'G-Q77MF7PTHE',
+    apiKey: '',
+    authDomain: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: '',
+    appId: '',
+    measurementId: '',
   },
 };
